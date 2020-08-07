b9e9459ed0ee883da4b7680c290a4b4dd3b4d477
adam <adamsiedlec@wp.pl>
2020-08-07 13:48:43 +0200

    I have deleted some unnecessary things

--- src/main/java/searcher/shopSearcher/sneakerShop/SneakerShopSearcher.java
+++ src/main/java/searcher/shopSearcher/sneakerShop/SneakerShopSearcher.java
@@ -12,23 +12,14 @@ import searcher.Searcher;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class SneakerShopSearcher implements Searcher {
 
-    // if url contains forbidden but common string and its length is lower that URL_BORDER_LENGTH, it should be removed
-    private static final int URL_BORDER_LENGTH = 40;
     final String ADDRESS = "https://sneakershop.pl";
     final int TIMEOUT = 5000;
     final String SHOP_NAME = "SneakerShop";
-    // Strings that cannot be contained by desirable links
-    final List<String> forbiddenList = List.of("faq", "dostawa", "polityka", "reklamacja", "marketing", "home", "tabela",
-            "zwrotu", "odziez", "polityka", "obuwie", "prywatnosci", "okazje", "akcesoria", "cart", "rozmiar",
-            "wishlist", "myaccount", "upominkowe", "activation", "firmy", "marki", "regulamin", "outfits");
-    final List<String> forbiddenButCommonList = List.of("sport");
 
     @Override
     public List<Offer> getOffers(String shoeName, boolean genderMale, String size) {
@@ -104,69 +95,4 @@ public class SneakerShopSearcher implements Searcher {
         return offers;
     }
 
-    private String getSizeParam(String size) {
-        int defaultParam = 771; // for 47.5 EU size, 772 for 48
-        if (NumberUtils.isCreatable(size)) {
-            float sizeFloat = Float.parseFloat(size);
-            if (sizeFloat < 47.5) {
-                while (sizeFloat != 47.5) {
-                    sizeFloat = sizeFloat + 0.5f;
-                    defaultParam--;
-                }
-                return "" + defaultParam;
-            } else if (sizeFloat > 47.5) {
-                while (sizeFloat != 47.5) {
-                    sizeFloat = sizeFloat - 0.5f;
-                    defaultParam++;
-                }
-                return "" + defaultParam;
-            } else {
-                return "" + defaultParam;
-            }
-        } else {
-            System.out.println("Size is not creatable");
-        }
-        return "";
-    }
-
-
-    private List<URL> getListWithoutForbidden(Set<URL> urls) {
-        List<URL> urlList = new ArrayList<>(urls);
-        List<URL> urlsToDelete = new ArrayList<>();
-        for (int i = 0; i < urlList.size(); i++) {
-            URL url = urlList.get(i);
-            for (String forbidden : forbiddenList) {
-                if (url.toString().contains(forbidden)) {
-                    //urlList.remove(i);
-                    urlsToDelete.add(url);
-                    break;
-                }
-
-            }
-            for (String forbiddenButCommon : forbiddenButCommonList) {
-                if (url.toString().contains(forbiddenButCommon) && url.toString().length() < URL_BORDER_LENGTH) {
-                    urlsToDelete.add(url);
-                    break;
-                }
-
-            }
-        }
-        for (URL url : urlsToDelete) {
-            urlList.remove(url);
-        }
-        return urlList;
-    }
-
-    private List<URL> getShortLinksAway(List<URL> urls) {
-        List<URL> urlsToDelete = new ArrayList<>();
-        for (URL url : urls) {
-            if (url.toString().length() < URL_BORDER_LENGTH) {
-                urlsToDelete.add(url);
-            }
-        }
-        for (URL url : urlsToDelete) {
-            urls.remove(url);
-        }
-        return urls;
-    }
 }
